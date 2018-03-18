c775d1997d243e203e96cde1f0df470bac40b799
Stefan Berger <stefanb@linux.vnet.ibm.com>
2018-03-18 19:23:59 -0400

    tpm2: TPM_MainInit write permanent state blob if SetState() blob was used
    If the permanent state was set using SetState() write the permanent
    state once we successfully read the volatile state and can use it.
    This way we have the state in a file.
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>

--- src/tpm_tpm2_interface.c
+++ src/tpm_tpm2_interface.c
@@ -93,6 +93,7 @@ TPM_BOOL _TPM2_CheckNVRAMFileExists(void)
 TPM_RESULT TPM2_MainInit(void)
 {
     TPM_RESULT ret = TPM_SUCCESS;
+    bool has_cached_state;
 
     g_inFailureMode = FALSE;
 
@@ -119,6 +120,8 @@ TPM_RESULT TPM2_MainInit(void)
         TPM_Manufacture(TRUE);
     }
 
+    has_cached_state = HasCachedState(TPMLIB_STATE_PERMANENT);
+
     _rpc__Signal_PowerOn(FALSE);
 
     _rpc__Signal_NvOn();
@@ -126,6 +129,10 @@ TPM_RESULT TPM2_MainInit(void)
     if (ret == TPM_SUCCESS) {
         if (g_inFailureMode)
             ret = TPM_RC_FAILURE;
+    }
+
+    if (ret == TPM_SUCCESS && has_cached_state) {
+        NvCommit();
     }
 
     return ret;
